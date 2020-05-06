6096c2f7e3cfcc6ec262f3a70d9b2cc461da12c2
name <email@email.fr>
2020-05-06 15:03:30 +0200

    Refactor arborescence with all features
    login, logout, register, errors and an index

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from config import Config
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    #from app import routes, models
+
+    app.app_context().push()
+
+    return app
+
