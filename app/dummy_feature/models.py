14cfdddac6210febd905abbb2bb5f2fd506a3e25
mvrvin <pinto.marvin1995@gmail.com>
2020-05-18 20:32:35 +0200

    Update data model to remove environment table and references, add feature table.

--- app/dummy_feature/models.py
+++ app/dummy_feature/models.py
@@ -1,19 +1,6 @@
 from app import db
 from sqlalchemy.sql import func
 
-# Model for the environment table
-class Environment(db.Model):
-	# Set the table name
-	__tablename__ = 'environment'
-
-	# Set table columns
-	id = db.Column(db.Integer, primary_key=True)
-	label = db.Column(db.String(50), unique=True)
-	rooms = db.relationship('Room', backref='environment', lazy=True)
-
-	def __repr__(self):
-		return '<Environment {}>'.format(self.label)
-
 # Model for the room table
 class Room(db.Model):
 	# Set the table name
@@ -23,11 +10,23 @@ class Room(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(30), unique=True)
 	area = db.Column(db.Float, nullable=True)
-	environment_id = db.Column(db.Integer, db.ForeignKey('environment.id'), nullable=False)
-	temperature_datas = db.relationship('TemperatureData', backref='room', lazy=True)
+	sensors = db.relationship('Sensor', backref='room', lazy=True)
 
 	def __repr__(self):
 		return '<Room {}>'.format(self.name)
+
+# Model for the sensor type table
+class SensorType(db.Model):
+	# Set the table name.
+	__tablename__ = 'sensor_type'
+
+	# Set table columns.
+	id = db.Column(db.Integer, primary_key=True)
+	label = db.Column(db.String(100), unique=True, nullable=False)
+	sensors = db.relationship('Sensor', backref='sensor_type', lazy=True)
+
+	def __repr__(self):
+		return '<Sensor Type {}>'.format(self.label)
 
 # Model for the sensor table
 class Sensor(db.Model):
@@ -38,17 +37,13 @@ class Sensor(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50), unique=True)
 	active = db.Column(db.Boolean, server_default="1")
+	sensor_type_id = db.Column (db.Integer, db.ForeignKey('sensor_type.id'), nullable=False)
+	room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
 	temperature_datas = db.relationship('TemperatureData', backref='sensor', lazy=True)
+	features = db.relationship('Feature', backref='sensor', lazy=True)
 
 	def __repr__(self):
 		return '<Sensor {}>'.format(self.name)
-
-# Model for the association table between tables "sensor" and "room".
-sensorRoom = db.Table(
-	'sensor_room',
-	db.Column('sensor_id', db.Integer, db.ForeignKey('sensor.id'), primary_key=True),
-	db.Column('room_id', db.Integer, db.ForeignKey('room.id'), primary_key=True)
-)
 
 # Model for the temperature's data table
 class TemperatureData(db.Model):
@@ -60,4 +55,26 @@ class TemperatureData(db.Model):
 	value = db.Column(db.Float, nullable=False)
 	snapshot_date = db.Column(db.DateTime, server_default=func.now())
 	sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable=False)
-	room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+
+	def __repr__(self):
+		return '<Temperature data {}>'.format(self.value)
+
+# Model for feature table.
+class Feature(db.Model):
+	# Set the table name.
+	__tablename__ = 'feature'
+
+	# Set table columns.
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	enum_type = db.Column(db.Enum('type','other'), nullable=False)
+
+	def __repr__(self):
+		return '<Feature {}>'.format(self.name)
+
+# Model for the association table between tables "sensor" and "feature".
+sensorFeature = db.Table(
+	'sensor_feature',
+	db.Column('sensor_id', db.Integer, db.ForeignKey('sensor.id'), primary_key=True),
+	db.Column('feature_id', db.Integer, db.ForeignKey('feature.id'), primary_key=True)
+)
--- app/dummy_feature/routes.py
+++ app/dummy_feature/routes.py
@@ -3,46 +3,22 @@ from flask_login import current_user, login_required
 
 from app import db
 from app.dummy_feature import bp
-from app.dummy_feature.forms import EnvironmentForm, RoomForm
-from app.dummy_feature.models import Environment, Room
+from app.dummy_feature.forms import RoomForm
+from app.dummy_feature.models import Room
 
 # Main page.
 @bp.route('/dummy_page')
 @login_required
 def dummy_page():
-    # Retrieve environments.
-    environments = Environment.query.all()
+    # Retrieve rooms.
     rooms = Room.query.all()
 
     # Render the page
     return render_template(
         'dummy_feature/dummy_page.html', 
         title='Dummy Title', 
-        environments=environments,
         rooms=rooms
-    )
-
-# Method to add an environment.
-@bp.route('/environment/add', methods=['GET', 'POST'])
-@login_required
-def add_environment():
-    # If user is not authenticated, redirect to login page.
-    if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
-    
-    form = EnvironmentForm()
-    
-    # If form is correctly filled, add in database.
-    # Redirect to main page.
-    if form.validate_on_submit():
-        environment = Environment(label=form.label.data)
-        db.session.add(environment)
-        db.session.commit()
-        flash('Congratulations, you added a new environment!')
-        return redirect(url_for('dummy_feature.dummy_page'))
-    
-    # Redirect to form to add environment.
-    return render_template('dummy_feature/add_environment.html', title='Add environment', form=form)    
+    )  
 
 # Method to add a room.
 @bp.route('/room/add', methods=['GET', 'POST'])
@@ -54,17 +30,12 @@ def add_room():
 
     form = RoomForm()
 
-    # Retrieve all environments to fill dropdown list.
-    environments = Environment.query.order_by('label')
-    form.environments.choices = [(e.id, e.label) for e in environments]
-
     # If form is correctly filled, add in database.
     # Redirect to main page.
     if form.validate_on_submit():
         room = Room(
             name=form.name.data, 
-            area=form.area.data, 
-            environment_id=form.environments.data
+            area=form.area.data
         )
         db.session.add(room)
         db.session.commit()
