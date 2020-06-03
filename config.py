ae1e732b72ada7c73fefceef84b1ff367cf1d920
name <email@email.fr>
2020-06-03 14:41:05 +0200

    Add Logger to project configurations

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
+import logging
+import logging.config
+import yaml
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
@@ -13,3 +17,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    with open('config.yaml', 'r') as f:
+        conf = yaml.safe_load(f.read())
+        print(logging.__file__)
+        logging.config.dictConfig(conf)
+
+    logger = logging.getLogger(__name__)
+
+    logger.debug('This is a debug message')
