275e4274b40dc104f4f61d9defcfd1d3a799287c
NicolasFortil <64467791+NicolasFortil@users.noreply.github.com>
2020-06-10 14:35:29 +0200

    Normalize app/auth/routes.py to respect import alphabetical and thirdparty/project import.

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,12 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-
-#from app.auth.forms import LoginForm, RegistrationForm
-from app.auth.forms import LoginForm, RegistrationForm
-from app.auth.models import User
 from werkzeug.urls import url_parse
+
 from app import db
 from app.auth import bp
+from app.auth.forms import LoginForm, RegistrationForm
+from app.auth.models import User
 
 
 # Login page route
