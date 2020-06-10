34c6c6f1f8397d0a5b60fcf14546bca44da77bef
NicolasFortil <64467791+NicolasFortil@users.noreply.github.com>
2020-06-10 15:11:05 +0200

    Remove deprecated code from app/feature/routes

--- app/features/routes.py
+++ app/features/routes.py
@@ -14,7 +14,6 @@ from app import db
 @bp.route('/temperature')
 @login_required
 def temperature():
-    feature_one = Feature.query.get(1)
     sensor_number = request.args.get('sensor', default=None, type=int)
     sensor = None
     temp = None
@@ -28,35 +27,11 @@ def temperature():
         time = []
         date_format = '%Y-%m-%d %H:%M:%S'
         for a in temperature_plus:
-            # a_temp = {'value': a.value, 'snapshot_date': a.snapshot_date}
             temp.append(a.value)
             time.append(a.snapshot_date.strftime(date_format))
     else:
         sensor_number = None
 
-
-
-    # Render the page
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    # [12, 19, 3, 5, 2, 3]['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']'# of Votes'
-
-    #room_one = Room.query.get(1)
-    #sensor_one = Sensor.query.get(1)
-    #if sensor_one is None:
-        #sensor_one_type = SensorType.query.get(1)
-        #if sensor_one_type is None:
-            #sensor_one_type = SensorType(label="DummyLebal")
-            #db.session.add(sensor_one_type)
-            #db.session.commit()
-        #sensor_one = Sensor(name="SensorToilette", sensor_type_id=sensor_one_type.id, room_id=room_one.id)
-        #db.session.add(sensor_one)
-        #db.session.commit()
-    #temperature_plus = TemperatureData(value=random.randint(42,69), sensor_id=sensor_one.id)
-    #db.session.add(temperature_plus)
-    #db.session.commit()
-    #temperature_plus = json.dumps(list(temperature_plus))
-    return render_template('features/temperature.html', title='Temperature', values=values, labels=labels,
-                           legend=legend, sensors=sensors, sensor=sensor, feature=feature_one, temperature=temp,
+    return render_template('features/temperature.html', title='Temperature', sensors=sensors,
+                           sensor=sensor, feature=feature_one, temperature=temp,
                            timestamps=time, sensor_number=sensor_number)
