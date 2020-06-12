345193bbe47b14079dd6a4337cb8b6d5e9b6a689
mvrvin <pinto.marvin1995@gmail.com>
2020-06-12 11:45:55 +0200

    Update init and config files to handle connection with a PostgreSQL's database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,47 +1,36 @@
-from config import Config
+import os
+
 from flask import Flask
 from flask_login import LoginManager
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-# Setup Database and login view
-db = SQLAlchemy()
-migrate = Migrate()
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 login = LoginManager()
+login.init_app(app)
 login.login_view = 'auth.login'
 
 
-# Create app Function. Initiate all routes and configs
-def create_app(config_class=Config):
-    # Create empty flask app and link with config
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+from app.models import *
 
-    # Link database and login to the app
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login.init_app(app)
 
-    # Import errors module
-    from app.errors import bp as errors_bp
-    app.register_blueprint(errors_bp)
+# Import authentication module
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    # Import authentication module
-    from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    # Import Features module
-    from app.features import bp as features_bp
-    app.register_blueprint(features_bp, url_prefix='/features')
+# Import authentication module
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
-    # Import Dummy_Feature module
-    from app.dummy_feature import bp as dummy_bp
-    app.register_blueprint(dummy_bp, url_prefix='/dummy_feature')
 
-    # Import management module.
-    from app.management import bp as management_bp
-    app.register_blueprint(management_bp, url_prefix='/management')
+# Import authentication module
+from app.features import bp as features_bp
+app.register_blueprint(features_bp, url_prefix='/features')
 
-    # Push application context and return
-    app.app_context().push()
-    return app
+
+# Import authentication module
+from app.management import bp as management_bp
+app.register_blueprint(management_bp, url_prefix='/management')
--- config.py
+++ config.py
@@ -10,19 +10,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 
-# Project environment variable. Set in .flaskenv
 class Config(object):
-    # Encryption secret key
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    # Set DB
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    with open('config.yaml', 'r') as f:
-        conf = yaml.safe_load(f.read())
-        print(logging.__file__)
-        logging.config.dictConfig(conf)
+	CSRF_ENABLED = True
+	DEBUG = True
+	TESTING = False
+	SECRET_KEY = os.environ['SECRET_KEY']
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	with open('config.yaml', 'r') as f:
+		conf = yaml.safe_load(f.read())
+		print(logging.__file__)
+		logging.config.dictConfig(conf)
 
-    logger = logging.getLogger(__name__)
+	logger = logging.getLogger(__name__)
 
-    logger.debug('This is a debug message')
+	logger.debug('This is a debug message')
