12d39af4b33a2d9f4beecfed1175e5133eed2067
mvrvin <pinto.marvin1995@gmail.com>
2020-06-12 11:44:17 +0200

    [Refactoring] Replace spaces by tabs.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,32 +7,32 @@ from app.models import User
 
 # Registration form to create a ew user
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField(
+		'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different username.')
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different username.')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Please use a different email address.')
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different email address.')
 
 
 # Login form for connection
 class LoginForm(FlaskForm):
-    username_style={'class':'form-control', 'placeholder':'Username'}
-    password_style={'class':'form-control', 'placeholder':'Password'}
-    remember_me_style={'class':'custom-control-input'}
-    submit_style={'class':'btn form-btn'}
+	username_style={'class':'form-control', 'placeholder':'Username'}
+	password_style={'class':'form-control', 'placeholder':'Password'}
+	remember_me_style={'class':'custom-control-input'}
+	submit_style={'class':'btn form-btn'}
 
-    username = StringField('Username', validators=[DataRequired()], render_kw=username_style)
-    password = PasswordField('Password', validators=[DataRequired()], render_kw=password_style)
-    remember_me = BooleanField('Remember Me', render_kw=remember_me_style)
-    submit = SubmitField('Sign in', render_kw=submit_style)
+	username = StringField('Username', validators=[DataRequired()], render_kw=username_style)
+	password = PasswordField('Password', validators=[DataRequired()], render_kw=password_style)
+	remember_me = BooleanField('Remember Me', render_kw=remember_me_style)
+	submit = SubmitField('Sign in', render_kw=submit_style)
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -11,52 +11,52 @@ from app.models import User
 # Login page route
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    # If already authenticated, redirect to the index
-    if current_user.is_authenticated:
-        return redirect(url_for('management.dashboard'))
-    # Get the login Form
-    form = LoginForm()
-    # When form is filled, get user and password and see if they exist
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        # Redirect to login with an error if incorrect user or password
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
-        # Login the user and redirect to the target page or the index
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('management.dashboard')
-        return redirect(next_page)
-    # Render login page with form to fill
-    return render_template('auth/login.html', title='Sign In', form=form)
+	# If already authenticated, redirect to the index
+	if current_user.is_authenticated:
+		return redirect(url_for('management.dashboard'))
+	# Get the login Form
+	form = LoginForm()
+	# When form is filled, get user and password and see if they exist
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		# Redirect to login with an error if incorrect user or password
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('auth.login'))
+		# Login the user and redirect to the target page or the index
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('management.dashboard')
+		return redirect(next_page)
+	# Render login page with form to fill
+	return render_template('auth/login.html', title='Sign In', form=form)
 
 
 # Logout page, redirect to login page
 @bp.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
+	logout_user()
+	return redirect(url_for('auth.login'))
 
 
 # Register page
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    # Redirect to the index if already authetified
-    if current_user.is_authenticated:
-        return redirect(url_for('management.dashboard'))
-    # If the registration form is filled by the user, create user and redirect to login page
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('auth.login'))
-    # Render the registration page with form to fill
-    return render_template('auth/register.html', title='Register', form=form)
+	# Redirect to the index if already authetified
+	if current_user.is_authenticated:
+		return redirect(url_for('management.dashboard'))
+	# If the registration form is filled by the user, create user and redirect to login page
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congratulations, you are now a registered user!')
+		return redirect(url_for('auth.login'))
+	# Render the registration page with form to fill
+	return render_template('auth/register.html', title='Register', form=form)
 
 
 # Index home page
@@ -64,8 +64,8 @@ def register():
 @bp.route('/index')
 @login_required
 def index():
-    if current_user.is_authenticated:
-        user = current_user.username
-    else:
-        user = 'anonymous'
-    return render_template('auth/index.html', title='SusHouse_v0.0.1', user=user)
+	if current_user.is_authenticated:
+		user = current_user.username
+	else:
+		user = 'anonymous'
+	return render_template('auth/index.html', title='SusHouse_v0.0.1', user=user)
