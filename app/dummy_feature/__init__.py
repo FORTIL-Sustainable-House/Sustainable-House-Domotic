fa53fe7588f84910aff837f436eb82233a14ffec
mvrvin <pinto.marvin1995@gmail.com>
2020-06-12 11:36:41 +0200

    Remove folders & files about "dummy_feature".

--- app/dummy_feature/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Blueprint loaded by main app to get the routes
-bp = Blueprint('dummy_feature', __name__, template_folder='templates')
-
-# Import routes
-from app.dummy_feature import routes
